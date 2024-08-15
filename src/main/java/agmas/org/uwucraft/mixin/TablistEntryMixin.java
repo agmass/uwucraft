20fc0c086cbf3e6e5c1baed71ce2540f63f82209
agmas <Maxlion360@gmail.com>
2024-08-15 17:06:39 +0400

    Update TablistEntryMixin.java

--- src/main/java/agmas/org/uwucraft/mixin/TablistEntryMixin.java
+++ src/main/java/agmas/org/uwucraft/mixin/TablistEntryMixin.java
@@ -2,35 +2,31 @@ package agmas.org.uwucraft.mixin;
 
 
 import agmas.org.uwucraft.UwUtils;
-import com.mojang.authlib.GameProfile;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.encryption.PublicPlayerSession;
+import net.minecraft.client.network.PlayerListEntry;
 import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.text.Text;
-import net.minecraft.world.GameMode;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.UUID;
+import java.util.List;
 
-@Mixin(PlayerListS2CPacket.class)
+@Mixin(PlayerListEntry.class)
 public class TablistEntryMixin {
 
-    @Mutable
-    @Shadow @Final
-    private Text displayName;
 
-    @Inject(method = "apply(Lnet/minecraft/network/listener/ClientPlayPacketListener;)V", at = @At("HEAD"))
-    private void injected(ClientPlayPacketListener clientPlayPacketListener, CallbackInfo ci) {
-        if (displayName.getLiteralString() != null)
-            this.displayName = Text.of(UwUtils.maximumUwuify(displayName.getLiteralString()));
+    @Shadow @Nullable private Text displayName;
+
+    @Inject(method = "setDisplayName", at = @At("HEAD"))
+    private void injected(Text displayName, CallbackInfo ci) {
+        if (displayName != null)
+            if (displayName.getLiteralString() != null)
+                this.displayName = Text.of(UwUtils.maximumUwuify(displayName.getLiteralString()));
     }
 }
